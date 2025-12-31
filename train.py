13561a261e834762b57258249d2dae251074f1e2
Aswin Balasubramanian <b.aswin32@gmail.com>
2025-12-31 22:01:11 +0530

    ticket cancellation feature

--- train.py
+++ train.py
@@ -1,18 +1,28 @@
 
 print("Welcome to Train Ticket Booking System")
+seats=0
 while True:
 
-    print("1. Available Trains" );
-    print("2. Book Ticket");
-    print("3. Ticket cancellation");
-    print("4. Exit");
-    print("Enter your choice: ");
-    choice = int(input("Choice: "));
+    print("1. Available Trains" )
+    print("2. Book Ticket")
+    print("3. Ticket cancellation")
+    print("4. Exit")
+    print("Enter your choice: ")
+    choice = int(input("Choice: "))
     if(choice == 1):
-        print("Available Trains:");
-        print("704 - Intercity Express - Seats: 60");
+        seat = 60
+        print("----------------------")
+        print("Available Trains:")
+        print("704 - Intercity Express - From City goa to City mumbai")
+        print("Departure Time: 10:00 AM")
+        print("Arrival Time: 04:00 PM")
+        seat=60-seats
+        print("Total Seats: ", seat)
+        print("----------------------")
 
     elif choice == 2:
+        print("----------------------")
+        print("Book Ticket")
         train_no = int(input("Enter train number: "))
         print("total seats:60")
         seats = int(input("Enter number of seats to book: "))
@@ -22,16 +32,21 @@ while True:
             print("Booking failed")
         rem=60 - seats
         print("Remaining seats:", rem) 
+        print("----------------------")
     elif choice == 3:
+        print("----------------------")
+        print("Ticket Cancellation")
         train_no = int(input("Enter train number: "))
         seats = int(input("Enter seats to cancel: "))
         if train_no == 704:
             print("Ticket cancelled successfully!")
-            rem=60 + seats
+            rem=60 -seats
             print("Remaining seats:", rem) 
         else:
             print("Cancellation failed")
+        print("----------------------")    
     else:
         print("Exiting the system. Thank you!")
+        print("----------------------")
         break
                   
