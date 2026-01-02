00add8c9b4d1de2c4321e650406bf3ee14b58dd6
Anjana <anjana27max@gmail.com>
2026-01-02 16:29:57 +0530

    asking names

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 
 print("Welcome to Train Ticket Booking System")
-seats=0
+total=60
+seats=total
 while True:
 
     print("1. Available Trains" )
@@ -10,29 +11,41 @@ while True:
     print("Enter your choice: ")
     choice = int(input("Choice: "))
     if(choice == 1):
-        seat = 60
         print("----------------------")
         print("Available Trains:")
-        print("704 - Intercity Express - From City goa to City mumbai")
+        print("704 - Intercity Express - From Goa to Mumbai")
         print("Departure Time: 10:00 AM")
         print("Arrival Time: 04:00 PM")
         seat=60-seats
-        print("Total Seats: ", seat)
+        print("Total Seats: ", seats)
         print("----------------------")
 
     elif choice == 2:
         print("----------------------")
         print("Book Ticket")
         train_no = int(input("Enter train number: "))
-        print("total seats:60")
-        seats = int(input("Enter number of seats to book: "))
-        if train_no == 704 and seats <= 60:
+        if train_no == 704 and seats >0:
+            print("Available seats: ",seats)
+            n=int(input("Number of passenger to book a ticket: "))
+            if n<=seats and n>0:
+                names=[]
+                for i in range(n):
+                    while True:
+                        name=input(f"Enter name {i+1}: ")
+                        if name.isalpha():
+                                names.append(name)
+                                break
+                        else:
+                                print("INVALID NAME !, Use only letters")  
+            seats-=n
+            print("Collected names  ",names)     
+            print("Total seats booked: ",n)
             print("Ticket booked successfully!")
+            print("Remaining seats:", seats) 
+            print("----------------------")
         else:
-            print("Booking failed")
-        rem=60 - seats
-        print("Remaining seats:", rem) 
-        print("----------------------")
+            print("Invalid train number")
+            print("----------------------")
     elif choice == 3:
         print("----------------------")
         print("Ticket Cancellation")
